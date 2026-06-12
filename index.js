a3c743beb80da6c18faf3f656a4e71aef4a08e9f
DaveSteffan <dave.steffan@gmail.com>
2026-06-12 14:33:15 -0400

    removed logging, changed button

--- index.js
+++ index.js
@@ -20,7 +20,6 @@ container.appendChild(destination_button);
 setInterval(() => {
     tick_counter += 1;
     tickchange();
-    console.log("finished tick");
 }, 250);
 
 function move() {
@@ -28,16 +27,12 @@ function move() {
     // NOTE: uses the same check roll for all of these, possible that is wrong but it will save processing
     var skillcheck = Math.floor(Math.random() * 100) + 1;
     if (skillcheck < (movement_skill + (physical_stat * .1))) {
-        console.log("movement success");
         if(current_destination) {
             distnace_to_destination -= (100.00 - skillcheck) * (movement_skill / 100.00); // i think this is right 
         }
-    } else {
-        console.log("movement failed");
     }
     // check for skill growth
     if ((skillcheck < (100.00 + movement_skill_pity) - movement_skill )) {
-        console.log("movement skill growth");
         movement_skill_pity = 0.00;
         movement_skill += ((101.00 - movement_skill) / 100.00) * ((101.00 - physical_stat) / 100.00) + ((Math.random() * 5) / 10.0);
     } else {
@@ -46,14 +41,11 @@ function move() {
     // check for stat growth
     // the 50 halves growth, and the *4 makes it have a harsher curve than the skills
     if ((physical_stat < (50.00 + physical_stat_pity) - physical_stat * 4)) {
-        console.log("movement skill growth");
         physical_stat_pity = 0.00;
         physical_stat += ((101.00 - physical_stat) / 100.00) * ((101.00 - physical_stat) / 100.00) + ((Math.random() * 5) / 10.0);
     } else {
         physical_stat_pity += 1.00;
     }
-
-
 }
 
 function tickchange() {
@@ -70,7 +62,6 @@ function tickchange() {
     if(current_destination) {
         bottom_label.textContent = "Bearing: " + current_destination + " Distance: " + distnace_to_destination.toFixed(2);
     }
-    console.log("end of tickchange")
 }
 
 function setcourse(destination_name) {
@@ -78,7 +69,7 @@ function setcourse(destination_name) {
         current_destination = destination_name;
         if(current_destination == "Asteroid") {
             distnace_to_destination = asteroid_distance;
-            destination_button.remove();
+            destination_button.style.display = 'none';
         }
     }
 
