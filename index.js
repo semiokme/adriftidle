80bc4cfb0bef5d59ebfc6148bc8fa9b6fc79dcb7
DaveSteffan <dave.steffan@gmail.com>
2026-06-12 14:41:46 -0400

    test

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ var asteroid_distance = 10000.00;
 var destination_button = document.createElement('button')
 destination_button.textContent = "Asteroid";
 destination_button.id = "destbutton";
-destination_button.addEventListener("click", setcourse(destination_button.innerText))
+destination_button.onclick = setcourse(destination_button.innerText)
 
 var container = document.getElementById("buttonContainer");
 container.appendChild(destination_button);
